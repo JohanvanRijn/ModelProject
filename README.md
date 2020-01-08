7012b1a9ca026382a802cd72f17d8fd351a409c1
Johan <johanvrijn0259@outlook.com>
2020-01-08 19:30:49 +0100

    Test

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# TvDrifting
-Small Project
+# Model Maker
+Small Project for automating the anoing task of creating Models and Tables in Laravel.
--- js/index.js
+++
@@ -1,8 +0,0 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-
-ctx.beginPath();
-ctx.lineWidth = "6";
-ctx.strokeStyle = "red";
-ctx.rect(5, 5, 290, 140);
-ctx.stroke();
